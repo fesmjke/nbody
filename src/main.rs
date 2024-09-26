96511c3934a314e3efa94d3e2c913b354628232f
fesmjke <fatalerror.sm@gmail.com>
2024-09-26 18:14:54 +0000

    feat: added entry point of drawing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
-    println!("Hello, world!");
+use macroquad::prelude::*;
+
+#[macroquad::main("N-Body")]
+async fn main() {
+    let night_blue= Color::from_rgba(0, 0, 25, 255);
+    loop {
+        clear_background(night_blue);                 
+        next_frame().await
+    }
 }
