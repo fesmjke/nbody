d3488cad0f8ab2359e7848cfe69fe8a94aac454d
fesmjke <fatalerror.sm@gmail.com>
2024-09-27 20:01:18 +0000

    feat: added rand vec and rand pos helpers functions

--- src/helper.rs
+++ src/helper.rs
@@ -1,6 +1,20 @@
-use macroquad::{color::RED, shapes::draw_line};
+use macroquad::{color::RED, math::Vec2, rand::gen_range, shapes::draw_line};
 
 pub fn cross(w: f32, h: f32) {
     draw_line(w / 2.0, 0.0, w / 2.0, h, 1.0, RED);
     draw_line(0.0, h / 2.0, w, h / 2.0, 1.0, RED);
+}
+
+pub fn rand_vec2(min: f32, max: f32) -> Vec2 {
+    let x = gen_range(min, max);
+    let y = gen_range(min, max);
+
+    Vec2::new(x, y)
+}
+
+pub fn rand_pos(w: f32, h: f32) -> Vec2 {
+    let x = gen_range(0.0, w);
+    let y = gen_range(0.0, h);
+
+    Vec2::new(x, y)
 }
