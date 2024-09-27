5fbb2fb4c24b84899e25500743ce5a583ea5e373
fesmjke <fatalerror.sm@gmail.com>
2024-09-27 19:39:54 +0000

    fix: imported macroquad stuff

--- src/helper.rs
+++ src/helper.rs
@@ -1,3 +1,5 @@
+use macroquad::{color::RED, shapes::draw_line};
+
 pub fn cross(w: f32, h: f32) {
     draw_line(w / 2.0, 0.0, w / 2.0, h, 1.0, RED);
     draw_line(0.0, h / 2.0, w, h / 2.0, 1.0, RED);
