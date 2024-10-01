fceca90d9217aae0b2e35b8bee662660a36b3a9f
fesmjke <fatalerror.sm@gmail.com>
2024-10-01 17:12:21 +0000

    feat: updated main loop, added force apply by attraction

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use entity::{Draw, Entity};
 use helper::{cross, rand_mass, rand_pos, rand_vec2};
-use macroquad::prelude::*;
+use macroquad::{prelude::*, telemetry::enable};
 
 mod entity;
 mod helper;
@@ -14,31 +14,41 @@ async fn main() {
     let (width, height) = (screen_width(), screen_height());
 
     for _ in 0..quantity {
-        let velocity = rand_vec2(-0.01, 0.01);
-        let position = rand_pos(width, height );
+        let position = rand_pos(width , height );
         let mass = rand_mass(1.0, 5.0);
         let mut entity = Entity::default();
         entity.set_position(position);
-        entity.set_velocity(velocity);
         entity.set_mass(mass);
         entities.push(entity);
     }
 
     entities[0].toggle_debug();
 
-    let small_force = Vec2::new(0.0, 0.0001);
-        
     loop {
         clear_background(night_blue);        
         cross(width, height);
 
         // let delta_time = get_frame_time();
         println!("{:?}", entities[0]);
-        
-        for entity in entities.iter_mut() {
-            entity.draw();
-            entity.apply_force(small_force);
-            entity.moving();
+
+        for i in 0..entities.len() {
+            let mut force = Vec2::ZERO;
+            
+            for j in 0..entities.len() {
+                if i == j {
+                    continue;
+                }
+                
+                force = entities[j].force_attraction(&entities[i]);
+
+                if i == 0 {
+                    println!("attraction force to {:?} -> {:?}",j, force);
+                }
+                entities[i].apply_force(force);
+            }
+
+            entities[i].moving();
+            entities[i].draw();
         }
         
         next_frame().await
